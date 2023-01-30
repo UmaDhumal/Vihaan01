786ff99c92568f2a2ab5a421f54d0dbb11a84248
UmaDhumal <123440175+UmaDhumal@users.noreply.github.com>
2023-01-30 20:07:01 +0530

    by Uma

--- GDemo/src/com/Day01/A.java
+++ GDemo/src/com/Day01/A.java
@@ -1,12 +1,16 @@
 package com.Day01;
 
 public class A {
+	
+	public void m1()
+	{
+		System.out.println("Class A from Uma");
+	}
 
 	public static void main(String[] args) 
 	{
 		System.out.println("Class A from Vihaan");
 		
-
 	}
 
 }
