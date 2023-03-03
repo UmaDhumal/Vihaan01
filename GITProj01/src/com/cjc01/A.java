744a221909df561088298e563da50d456526f247
admin <admin@DESKTOP-6099UR4>
2023-03-03 23:23:08 +0530

    by uma

---
+++ GITProj01/src/com/cjc01/A.java
@@ -0,0 +1,15 @@
+package com.cjc01;
+
+public class A 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Class A");
+		
+	}
+	public void m1()
+	{
+		System.out.println("m1 by Uma");
+	}
+
+}
